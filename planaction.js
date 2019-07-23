9420f9718d6ca1940cc3a2f4cbbb30176cf6a15f
student <student@gwcadmins-MacBook-Pro-2.local>
2019-07-23 15:44:05 -0500

    jsonadded

---
+++ planaction.js
@@ -0,0 +1,120 @@
+var data = '{"week1":{"day1":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"5oz meat","side":"veggie"},"meal4":{"main":"3 servings veggies","side":"1 cup beans"},"snack4":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"]},"day2":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"chicken or turkey sandwich","side":"veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"soup or smoothie","side":"fruit or veggie"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}},"day3":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"chicken or turkey sandwich","side":"small salad"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"1/2 cup rice and veggie"}},"day4":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"soup - 300 cal","side":"small salad"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"smoothie - 300 cal","side":"fruit or veggie"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"1/2 sweet potato and veggie"}},"day5":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"soup - 300 cal","side":"small salad"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}},"day6":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal3":{"main":"5oz meat","side":"1/2 cup rice and veggies"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"large salad","side":"1 cup beans"},"snack4":["20 almonds","medium fruit","4 slices melba toast"]},"day7":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"medium salad","side":"piece of fruit"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}}},"week2":{"day1":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"5oz meat","side":"veggie"},"meal4":{"main":"3 servings veggies","side":"1 cup beans"},"snack4":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"]},"day2":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"chicken or turkey sandwich","side":"veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"soup or smoothie","side":"fruit or veggie"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}},"day3":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"chicken or turkey sandwich","side":"small salad"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"1/2 cup rice and veggie"}},"day4":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"soup - 300 cal","side":"small salad"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"smoothie - 300 cal","side":"fruit or veggie"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"1/2 sweet potato and veggie"}},"day5":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"soup - 300 cal","side":"small salad"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}},"day6":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal3":{"main":"5oz meat","side":"1/2 cup rice and veggies"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"large salad","side":"1 cup beans"},"snack4":["20 almonds","medium piece of fruit","4 slices melba toast"]},"day7":{"meal1":{"main":["egg white omelette or 2 egg whites","1.5 cups oatmeal","grilled cheese"],"side":"piece of fruit"},"snack1":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal2":{"main":"smoothie or soup - 300 cal","side":"fruit or veggie"},"snack2":["Bobo 150 cal Bar","50 goldfish","21 almonds","3 cups popcorn","1/2 avocado"],"meal3":{"main":"medium salad","side":"piece of fruit"},"snack3":["Chewy Bar","40 goldfish","14 almonds","2 cups popcorn","1/3 cup wasabi peas","hard-boiled egg"],"meal4":{"main":"5oz meat","side":"2 serving veggies"}}}}'
+
+var inner;
+
+function loadWeek(){
+  var val = document.getElementById('week').value;
+  var mydata = JSON.parse(data);
+  var innerSnacks = [];
+  switch(val){
+    case "week1":
+        week1 = mydata['week1'];
+        addWeek(week1);
+        document.getElementById('show').innerHTML = inner;
+        break;
+    case "week2":
+        week2 = mydata['week2'];
+        addWeek(week2);
+        document.getElementById('show').innerHTML = inner;
+        break;
+    case "week3":
+        document.getElementById('show').innerHTML = "you have selected week 3";
+        break;
+    case "week4":
+        document.getElementById('show').innerHTML = "you have selected week 4";
+        break;
+    case "week5":
+        document.getElementById('show').innerHTML = "you have selected week 5";
+        break;
+    case "week6":
+        document.getElementById('show').innerHTML = "you have selected week 6";
+        break;
+
+    }
+}
+
+function addSnacks(x){
+  if (x){
+
+    for (var j = 0; j < (x).length; j++){
+        inner = inner + "<option value='" + x[j]+"'>" + x[j]+"</option>";
+    }
+  }
+  else{
+    inner = inner + "<option value='none'>None</option>";
+  }
+}
+
+function addmeal1(x){
+  for (var j = 0; j < (x).length; j++){
+      inner = inner + "<option value='" + x[j]+"'>" + x[j]+"</option>";
+  }
+}
+
+function addWeek(x){
+  inner = "<table><tr><td></td><td>Day 1</td><td>Day 2</td><td>Day 3</td><td>Day 4</td><td>Day 5</td> <td>Day 6</td><td>Day 7</td></tr>"
+  inner = inner + "<td><b>Meal 1</b></td><td><p>" + x.day1.meal1.main[0]+ "</p><p>" + x.day1.meal1.side + "</p></td><td><p>" + x.day2.meal1.main[1]+ "</p><p>" + x.day2.meal1.side + "</p></td><td><p>" + x.day3.meal1.main[0]+ "</p><p>" + x.day3.meal1.side + "</p></td><td><p>" + x.day4.meal1.main[1]+ "</p><p>" + x.day4.meal1.side + "</p></td><td><p>" + x.day5.meal1.main[0]+ "</p><p>" + x.day5.meal1.side + "</p></td><td><p>" + x.day6.meal1.main[1]+ "</p><p>" + x.day6.meal1.side + "</p></td><td><p>" + x.day7.meal1.main[0]+ "</p><p>" + x.day7.meal1.side + "</p></td></tr>";
+  inner = inner + "<tr><td><b>Snack 1</b></td><td>" + "<select id = 'day1'>"
+  addSnacks(x.day1.snack1);
+  inner = inner + "</select></td><td><select id = 'day2'>"
+  addSnacks(x.day2.snack1);
+  inner = inner + "</select></td><td><select id = 'day3'>"
+  addSnacks(x.day3.snack1);
+  inner = inner + "</select></td><td><select id = 'day4'>"
+  addSnacks(x.day4.snack1);
+  inner = inner + "</select></td><td><select id = 'day5'>"
+  addSnacks(x.day5.snack1);
+  inner = inner + "</select></td><td><select id = 'day6'>"
+  addSnacks(x.day6.snack1);
+  inner = inner + "</select></td><td><select id = 'day7'>"
+  addSnacks(x.day7.snack1);
+  inner = inner + "</select></td></tr>"
+  inner = inner + "<td><b>Meal 2</b></td><td><p>" + x.day1.meal2.main+ "</p><p>" + x.day1.meal2.side + "</p></td><td><p>" + x.day2.meal2.main+ "</p><p>" + x.day2.meal2.side + "</p></td><td><p>" + x.day3.meal2.main+ "</p><p>" + x.day3.meal2.side + "</p></td><td><p>" + x.day4.meal2.main+ "</p><p>" + x.day4.meal2.side + "</p></td><td><p>" + x.day5.meal2.main+ "</p><p>" + x.day5.meal2.side + "</p></td><td><p>" + x.day6.meal2.main+ "</p><p>" + x.day6.meal2.side + "</p></td><td><p>" + x.day7.meal2.main+ "</p><p>" + x.day7.meal2.side + "</p></td></tr>";
+  inner = inner + "<tr><td><b>Snack 2</b></td><td>" + "<select id = 'day1s2'>"
+  addSnacks(x.day1.snack2);
+  inner = inner + "</select></td><td><select id = 'day2s2'>"
+  addSnacks(x.day2.snack2);
+  inner = inner + "</select></td><td><select id = 'day3s2'>"
+  addSnacks(x.day3.snack2);
+  inner = inner + "</select></td><td><select id = 'day4s2'>"
+  addSnacks(x.day4.snack2);
+  inner = inner + "</select></td><td><select id = 'day5s2'>"
+  addSnacks(x.day5.snack2);
+  inner = inner + "</select></td><td><select id = 'day6s2'>"
+  addSnacks(x.day6.snack2);
+  inner = inner + "</select></td><td><select id = 'day7s2'>"
+  addSnacks(x.day7.snack2);
+  inner = inner + "</select></td></tr>"
+  inner = inner + "<td><b>Meal 3</b></td><td><p>" + x.day1.meal3.main+ "</p><p>" + x.day1.meal3.side + "</p></td><td><p>" + x.day2.meal3.main+ "</p><p>" + x.day2.meal3.side + "</p></td><td><p>" + x.day3.meal3.main+ "</p><p>" + x.day3.meal3.side + "</p></td><td><p>" + x.day4.meal3.main+ "</p><p>" + x.day4.meal3.side + "</p></td><td><p>" + x.day5.meal3.main+ "</p><p>" + x.day5.meal3.side + "</p></td><td><p>" + x.day6.meal3.main+ "</p><p>" + x.day6.meal3.side + "</p></td><td><p>" + x.day7.meal3.main+ "</p><p>" + x.day7.meal3.side + "</p></td></tr>";
+  inner = inner + "<tr><td><b>Snack 3</b></td><td>" + "<select id = 'day1s3'>"
+  addSnacks(x.day1.snack3);
+  inner = inner + "</select></td><td><select id = 'day2s3'>"
+  addSnacks(x.day2.snack3);
+  inner = inner + "</select></td><td><select id = 'day3s3'>"
+  addSnacks(x.day3.snack3);
+  inner = inner + "</select></td><td><select id = 'day4s3'>"
+  addSnacks(x.day4.snack3);
+  inner = inner + "</select></td><td><select id = 'day5s3'>"
+  addSnacks(x.day5.snack3);
+  inner = inner + "</select></td><td><select id = 'day6s3'>"
+  addSnacks(x.day6.snack3);
+  inner = inner + "</select></td><td><select id = 'day7s3'>"
+  addSnacks(x.day7.snack3);
+    inner = inner + "</select></td></tr>"
+  inner = inner + "<td><b>Meal 4</b></td><td><p>" + x.day1.meal4.main+ "</p><p>" + x.day1.meal4.side + "</p></td><td><p>" + x.day2.meal4.main+ "</p><p>" + x.day2.meal4.side + "</p></td><td><p>" + x.day3.meal4.main+ "</p><p>" + x.day3.meal4.side + "</p></td><td><p>" + x.day4.meal4.main+ "</p><p>" + x.day4.meal4.side + "</p></td><td><p>" + x.day5.meal4.main+ "</p><p>" + x.day5.meal4.side + "</p></td><td><p>" + x.day6.meal4.main+ "</p><p>" + x.day6.meal4.side + "</p></td><td><p>" + x.day7.meal4.main+ "</p><p>" + x.day7.meal4.side + "</p></td></tr>";
+  inner = inner + "<tr><td><b>Snack 4</b></td><td>" + "<select id = 'day1s4'>"
+  addSnacks(x.day1.snack4);
+  inner = inner + "</select></td><td><select id = 'day2s4'>"
+  addSnacks(x.day2.snack4);
+  inner = inner + "</select></td><td><select id = 'day3s4'>"
+  addSnacks(x.day3.snack4);
+  inner = inner + "</select></td><td><select id = 'day4s4'>"
+  addSnacks(x.day4.snack4);
+  inner = inner + "</select></td><td><select id = 'day5s4'>"
+  addSnacks(x.day5.snack4);
+  inner = inner + "</select></td><td><select id = 'day6s4'>"
+  addSnacks(x.day6.snack4);
+  inner = inner + "</select></td><td><select id = 'day7s4'>"
+  addSnacks(x.day7.snack4);
+  inner = inner + "</select></td></tr>"
+}
