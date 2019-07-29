94bffd966956a9dc7df959fb0fd1de0bf056f287
student <student@gwcadmins-MacBook-Pro-2.local>
2019-07-29 12:34:05 -0500

    changes

---
+++ createList.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="myStyles.css">
+		<!-- <script src="data.json"></script> -->
+    <title>Shred Diet Planner</title>
+	</head>
+  <body>
+<div>
+<?php
+
+
+  $week = $_GET['week'];
+
+  switch(week){
+    case "week1":
+        echo "week is: " + week;
+        break;
+    case "week2":
+        echo "week is: " + week;
+        break;
+    case "week3":
+        echo "week is: " + week;
+        break;
+    case "week4":
+        echo "week is: " + week;
+        break;
+    case "week5":
+        echo "week is: " + week;
+        break;
+    case "week6":
+        echo "week is: " + week;
+        break;
+
+      }
+
+?></div>
+
+
+  </body>
+</html>
